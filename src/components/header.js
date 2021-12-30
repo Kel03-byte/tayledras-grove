dd79e497cdbecd6fa6c31dac8c186f475dc9e846
Kellie Ryan <littlekel03@gmail.com>
2021-12-30 16:17:32 +0800

    Added logo and some styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 // Header Component
 
 import React from 'react';
+import Logo from '../images/tg-logo.jpg'
 
 function Header () {
     return (
         <header>
-            <div>Tayledras Grove</div>
+            <img id="tg-logo" src={Logo} alt="Tayledras Grove Logo"></img>
         </header>
     );
 };
--- src/pages/for-sale/for-sale-page.js
+++ src/pages/for-sale/for-sale-page.js
@@ -7,44 +7,44 @@ function Sales() {
         <Fragment>
             <div id='page'>
                 <div id='title'>For Sale</div>
-                <div className='background' >Here you can find information about our Tayledras Ponies who are looking for their
+                <div>Here you can find information about our Tayledras Ponies who are looking for their
                     companions.
                 </div><br />
-                <div className='background' >Prices and more detailed information are available on enquiry.</div>
-                <div className='background' >
-                    <p className='background' >- Tayldras Dawnfire -</p>
-                    <div className='background'  id='info'>
-                        <p className='background' ><b><i className='background' >Stable Name:</i></b> Dawn</p>
-                        <p className='background' ><b><i className='background' >Year:</i></b> 2018</p>
-                        <p className='background' ><b><i className='background' >Gender:</i></b> Mare</p>
-                        <p className='background' ><b><i className='background' >Breed:</i></b> Connemara x Thoroughbred</p>
-                        <p className='background' ><b><i className='background' >Colour:</i></b> Bay</p>
-                        <p className='background' ><b><i className='background' >Sire:</i></b> Itsa Shamrock Forget Me Not (AUS)</p>
-                        <p className='background' ><b><i className='background' >Dam:</i></b> My Divine (AUS)</p>
+                <div>Prices and more detailed information are available on enquiry.</div>
+                <div>
+                    <p>- Tayldras Dawnfire -</p>
+                    <div id='info'>
+                        <p><b><i>Stable Name:</i></b> Dawn</p>
+                        <p><b><i>Year:</i></b> 2018</p>
+                        <p><b><i>Gender:</i></b> Mare</p>
+                        <p><b><i>Breed:</i></b> Connemara x Thoroughbred</p>
+                        <p><b><i>Colour:</i></b> Bay</p>
+                        <p><b><i>Sire:</i></b> Itsa Shamrock Forget Me Not (AUS)</p>
+                        <p><b><i>Dam:</i></b> My Divine (AUS)</p>
                     </div>
                 </div>
-                <div className='background' >
-                    <p className='background' >- Tayldras Raindance -</p>
-                    <div className='background' id='info'>
-                        <p className='background' ><b><i className='background' >Stable Name:</i></b> Rain</p>
-                        <p className='background' ><b><i className='background' >Year:</i></b> 2021</p>
-                        <p className='background' ><b><i className='background' >Gender:</i></b> Colt (to be gelded)</p>
-                        <p className='background' ><b><i className='background' >Breed:</i></b> Connemara x Thoroughbred</p>
-                        <p className='background' ><b><i className='background' >Colour:</i></b> Bay</p>
-                        <p className='background' ><b><i className='background' >Sire:</i></b> Itsa Shamrock Forget Me Not (AUS)</p>
-                        <p className='background' ><b><i className='background' >Dam:</i></b> My Divine (AUS)</p>
+                <div>
+                    <p>- Tayldras Raindance -</p>
+                    <div id='info'>
+                        <p><b><i>Stable Name:</i></b> Rain</p>
+                        <p><b><i>Year:</i></b> 2021</p>
+                        <p><b><i>Gender:</i></b> Colt (to be gelded)</p>
+                        <p><b><i>Breed:</i></b> Connemara x Thoroughbred</p>
+                        <p><b><i>Colour:</i></b> Bay</p>
+                        <p><b><i>Sire:</i></b> Itsa Shamrock Forget Me Not (AUS)</p>
+                        <p><b><i>Dam:</i></b> My Divine (AUS)</p>
                     </div>
                 </div>
-                <div className='background' >
-                    <p className='background' >- Tayldras Bon Kethry -</p>
-                    <div className='background' id='info'>
-                        <p className='background' ><b><i className='background' >Stable Name:</i></b> Kethry</p>
-                        <p className='background' ><b><i className='background' >Year:</i></b> 2020</p>
-                        <p className='background' ><b><i className='background' >Gender:</i></b> Filly</p>
-                        <p className='background' ><b><i className='background' >Breed:</i></b> Connemara x Warmblood</p>
-                        <p className='background' ><b><i className='background' >Colour:</i></b> Chestnut</p>
-                        <p className='background' ><b><i className='background' >Sire:</i></b> Itsa Shamrock Forget Me Not (AUS)</p>
-                        <p className='background' ><b><i className='background' >Dam:</i></b> Silversocks (AUS)</p>
+                <div>
+                    <p>- Tayldras Bon Kethry -</p>
+                    <div id='info'>
+                        <p><b><i>Stable Name:</i></b> Kethry</p>
+                        <p><b><i>Year:</i></b> 2020</p>
+                        <p><b><i>Gender:</i></b> Filly</p>
+                        <p><b><i>Breed:</i></b> Connemara x Warmblood</p>
+                        <p><b><i>Colour:</i></b> Chestnut</p>
+                        <p><b><i>Sire:</i></b> Itsa Shamrock Forget Me Not (AUS)</p>
+                        <p><b><i>Dam:</i></b> Silversocks (AUS)</p>
                     </div>
                 </div>
             </div>
